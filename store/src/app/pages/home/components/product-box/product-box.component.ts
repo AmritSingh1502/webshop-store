e066540957854b7e1b2cd5504562c8aa2e1ce90b
AmritSingh1502 <113255930+AmritSingh1502@users.noreply.github.com>
2024-06-02 22:40:42 +0530

    product-box component created

--- store/src/app/pages/home/components/product-box/product-box.component.ts
+++ store/src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+import { NgModule, Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-box',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      product-box works!
-    </p>
-  `,
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatListModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatBadgeModule,
+    MatSnackBarModule,],
+  templateUrl:'./product-box.component.html',
   styles: ``
 })
 export class ProductBoxComponent {
-
+@Input() fullWidthMode =false;
 }
